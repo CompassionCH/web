fb5475f8c9bc7cf3ccb425c5c0c6244fc7c9d4b3
Jonathan Tarabbia <j.tarabbia@gmail.com>
2019-03-27 16:56:47 +0100

    add compatibility tree view

--- web_widget_text_collapse_html/static/src/js/collapse.js
+++ web_widget_text_collapse_html/static/src/js/collapse.js
@@ -8,6 +8,9 @@ odoo.define("web_widget_text_collapse_html.collapse",
     // core._lt tool for allows term translation into the module
     var _lt = core._lt;
 
+    var Listview = require('web.ListView');
+    var formats = require('web.formats');
+
     //created a new field that inherits from the odoo standard fields
     var TextCollapse = core.form_widget_registry.get("html");
 
@@ -68,6 +71,16 @@ odoo.define("web_widget_text_collapse_html.collapse",
 
     // save the widget
     core.form_widget_registry.add('text_collapse_html',
-      TextCollapse);
+      textCollapseHTML);
+
+      Listview.Column.include({
+
+         _format: function (row_data, options) {
+
+      return formats.format_value(row_data[this.id].value, this, options.value_if_empty);
+
+         }
+
+     });
 
   });
